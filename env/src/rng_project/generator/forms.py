bc4b3105bc51131cf7e086e6272b8618ab9f5616
Tawfiq Said <79516981+ltlx4@users.noreply.github.com>
2021-09-27 14:44:16 +0200

    Delete env directory

--- env/src/rng_project/generator/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from random import randint
-
-class GuessForm(forms.Form):
-    human_guess = forms.IntegerField(label='Choose a number between 0-20', widget=forms.TextInput(attrs={'class' : 'form-control', 'type': 'number'}))
-
-
-    
--- env/src/rng_project/generator/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import Generator
-
-urlpatterns = [
-    path('', Generator.as_view(), name='index'),
-] 
